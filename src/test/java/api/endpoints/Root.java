4c9889291eba88c3f8b5a42206142e00134e8378
Patryk Prentki <patryk.prentki90@gmail.com>
2024-10-03 18:58:18 +0200

    Root class with user endpoints

---
+++ src/test/java/api/endpoints/Root.java
@@ -0,0 +1,11 @@
+package api.endpoints;
+
+public class Root {
+
+    public static String baseLink = "https://petstore.swagger.io/v2/";
+    public static String postUserLink = baseLink+"user";
+    public static String deleteUserLink = baseLink+"user/{username}";
+    public static String getUserLink = baseLink+"user/{username}";
+    public static String updateUserLink = baseLink+"user/{username}";
+
+}
